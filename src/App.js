670735c15556f3abf6c1bb709369122ffc89e62e
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2022-10-05 19:02:17 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
     <div >
       <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/Prueba-tecnica-IAF" element={<Home />} />
             <Route path="/:id" element={<SeccionPaciente />} />
             <Route path="/:id/:opcion" element={ <SeccionDetalles /> } />
-            <Route path="*" element={ <div>ERROR</div>} />
+            <Route path="*/" element={ <div>ERROR</div>} />
       </Routes>
     </div>
   );
